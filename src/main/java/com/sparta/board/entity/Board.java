7cfdda9b3922582d2c0b6bb66be2a91fcc77340d
doyeonKim <0915dy@gmail.com>
2023-02-05 00:18:19 +0900

    feat: #3 - Board entity 작성
    `Board` 엔티티와 작성일시,수정일시를 위한 `Timestamped` 클래스를 작성했다.
    Auditing 기능 사용을 위해 애플리케이션 `@EnableJpaAuditing`을 달아주었다.

---
+++ src/main/java/com/sparta/board/entity/Board.java
@@ -0,0 +1,27 @@
+package com.sparta.board.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@Entity
+@NoArgsConstructor
+public class Board extends Timestamped {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String contents;
+
+    @Column(nullable = false)
+    private String author;
+
+    @Column(nullable = false)
+    private String password;
+}
