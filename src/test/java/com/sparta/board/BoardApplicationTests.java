7d732317c9847d7f4fb087492ace46f7e5518916
doyeonKim <0915dy@gmail.com>
2023-02-04 20:07:08 +0900

    Initial commit

---
+++ src/test/java/com/sparta/board/BoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
