cbb99d43aa72f3583104b75d142cb8401ab334f1
doyeonKim <0915dy@gmail.com>
2023-02-05 11:47:28 +0900

    feat: #4 ResponseDto 수정
    Long id 대신 String title을 넣어주었다.
    Dto로 만들기 위한 생성자를 만들었다.

--- src/main/java/com/sparta/board/dto/BoardResponseDto.java
+++ src/main/java/com/sparta/board/dto/BoardResponseDto.java
@@ -3,10 +3,19 @@ package com.sparta.board.dto;
 import java.time.LocalDateTime;
 
 public class BoardResponseDto {
-    private Long id;
+    private String title;
     private String contents;
     private String author;
     private String password;
     private LocalDateTime createdAt;
     private LocalDateTime modifiedAt;
+
+    public BoardResponseDto(String title, String contents, String author, String password, LocalDateTime createdAt, LocalDateTime modifiedAt) {
+        this.title = title;
+        this.contents = contents;
+        this.author = author;
+        this.password = password;
+        this.createdAt = createdAt;
+        this.modifiedAt = modifiedAt;
+    }
 }
