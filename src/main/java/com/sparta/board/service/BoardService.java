90564d9ca7b5cd5c622e18b37bcb57fad61512fe
doyeonKim <0915dy@gmail.com>
2023-02-05 22:40:43 +0900

    feat: #18 responseDto 업데이트
    - `BoardResponseDto`에 `entity`를 넣으면 `dto`로 만들어주는 static method `from` 선언
    - `from` 메서드 이용하여 `BoardService`에서 각 함수 리턴시 간결하게 표현
    
    closes #18

--- src/main/java/com/sparta/board/service/BoardService.java
+++ src/main/java/com/sparta/board/service/BoardService.java
@@ -9,7 +9,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -20,27 +19,21 @@ public class BoardService {
 
     @Transactional(readOnly = true)
     public List<BoardResponseDto> getPosts() {
-        List<Board> posts = boardRepository.findAllByOrderByModifiedAtDesc();
-        List<BoardResponseDto> responseDtos = new ArrayList<>();
-        for (Board post : posts) {
-            responseDtos.add(new BoardResponseDto(post.getId(), post.getTitle(), post.getContents(), post.getAuthor(), post.getCreatedAt(), post.getModifiedAt()));
-        }
-        return responseDtos;
+        return boardRepository.findAllByOrderByModifiedAtDesc().stream().map(BoardResponseDto::from).toList();
     }
 
     @Transactional
     public BoardResponseDto createPost(BoardRequestsDto requestsDto) {
         Board board = new Board(requestsDto);
         boardRepository.save(board);
-        return new BoardResponseDto(board.getId(), board.getTitle(), board.getContents(), board.getAuthor(), board.getCreatedAt(), board.getModifiedAt());
+        return BoardResponseDto.from(board);
     }
 
     @Transactional
     public BoardResponseDto getPost(Long id) {
-        Board board = boardRepository.findById(id).orElseThrow(
+        return boardRepository.findById(id).map(BoardResponseDto::from).orElseThrow(
                 () -> new IllegalArgumentException("아이디가 존재하지 않습니다.")
         );
-        return new BoardResponseDto(board.getId(), board.getTitle(), board.getContents(), board.getAuthor(), board.getCreatedAt(), board.getModifiedAt());
     }
 
     @Transactional
@@ -52,7 +45,7 @@ public class BoardService {
             throw new Exception("비밀번호가 일치하지 않습니다.");
 
         board.update(requestsDto);
-        return new BoardResponseDto(board.getId(), board.getTitle(), board.getContents(), board.getAuthor(), board.getCreatedAt(), board.getModifiedAt());
+        return BoardResponseDto.from(board);
     }
 
     @Transactional
