d07602290189a3c1609bdd584415a1d377874332
doyeonKim <0915dy@gmail.com>
2023-02-10 23:08:57 +0900

    feat: #30 게시글 작성 기능 구현
    - `Board` 생성자 대신 `builder` 쓰기 위해 `@Builder` 추가했다.
    - `@Builder` 추가하기 위해 `@AllArgsConstructor`도 추가했다.
    - 글 작성 시, request header authorization 값을 확인해야 하므로
      `createPost`의 파라미터로 `HttpServletRequest`를 추가했다.
    - 작성된 게시글을 저장하기 전에, Token 값이 유효한지 확인 후 유효할 경우에만 저장한다.
    
    closes #30

--- src/main/java/com/sparta/board/controller/BoardController.java
+++ src/main/java/com/sparta/board/controller/BoardController.java
@@ -2,11 +2,11 @@ package com.sparta.board.controller;
 
 import com.sparta.board.dto.BoardRequestsDto;
 import com.sparta.board.dto.BoardResponseDto;
-import com.sparta.board.dto.SuccessResponseDto;
 import com.sparta.board.service.BoardService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
 @RestController
@@ -15,16 +15,19 @@ public class BoardController {
 
     private final BoardService boardService;
 
+    // 게시글 전체 목록 조회
     @GetMapping("/api/posts")
     public List<BoardResponseDto> getPosts() {
         return boardService.getPosts();
     }
 
+    // 게시글 작성
     @PostMapping("/api/post")
-    public BoardResponseDto createPost(@RequestBody BoardRequestsDto requestsDto) {
-        return boardService.createPost(requestsDto);
+    public BoardResponseDto createPost(@RequestBody BoardRequestsDto requestsDto, HttpServletRequest request) {
+        return boardService.createPost(requestsDto, request);
     }
 
+    // 선택된 게시글 조회
     @GetMapping("/api/post/{id}")
     public BoardResponseDto getPost(@PathVariable Long id) {
         return boardService.getPost(id);
@@ -35,9 +38,9 @@ public class BoardController {
         return boardService.updatePost(id, requestsDto);
     }
 
-    @DeleteMapping("/api/post/{id}")
-    public SuccessResponseDto deletePost(@PathVariable Long id, @RequestBody BoardRequestsDto requestsDto) throws Exception {
-        return boardService.deletePost(id, requestsDto);
-    }
+//    @DeleteMapping("/api/post/{id}")
+//    public SuccessResponseDto deletePost(@PathVariable Long id, @RequestBody BoardRequestsDto requestsDto) throws Exception {
+//        return boardService.deletePost(id, requestsDto);
+//    }
 
 }
