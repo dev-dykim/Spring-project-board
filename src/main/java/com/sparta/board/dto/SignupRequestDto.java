9f199b05a2d70b2dd58f7919d47478cab25db31f
doyeonKim <0915dy@gmail.com>
2023-02-13 00:23:32 +0900

    docs: #53 비밀번호 유효성 추가
    비밀번호에 특수문자도 입력 가능하도록 유효성 검사를 수정했다.
    입력가능한 특수문자는 !@#$%^&*()_+=?,./<>{}[]- 이다.

--- src/main/java/com/sparta/board/dto/SignupRequestDto.java
+++ src/main/java/com/sparta/board/dto/SignupRequestDto.java
@@ -14,7 +14,7 @@ public class SignupRequestDto {
     private String username;
 
     // 비밀번호 유효성 검사
-    @Pattern(regexp = "^[a-zA-Z0-9]{8,15}$", message = "비밀번호는 8~15자리 영문 대소문자(a~z, A~Z), 숫자(0~9)를 사용하세요!")
+    @Pattern(regexp = "^[a-zA-Z0-9~!@#$%^&*()_+=?,./<>{}\\[\\]\\-]{8,15}$", message = "비밀번호는 8~15자리 영문 대소문자(a~z, A~Z), 숫자(0~9), 특수문자를 사용하세요!")
     private String password;
 
     private Boolean admin;
