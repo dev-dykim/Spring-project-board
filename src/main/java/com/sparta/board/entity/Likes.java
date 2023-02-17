9073da4fe2dd7d2665cc8ba7e42ff7f14ff910da
doyeonKim <0915dy@gmail.com>
2023-02-17 20:33:25 +0900

    docs: #81 Likes 엔티티 추가
    좋아요 기능 추가에 따라 `Likes` 엔티티를 추가했다.

---
+++ src/main/java/com/sparta/board/entity/Likes.java
@@ -0,0 +1,30 @@
+package com.sparta.board.entity;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Likes {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "board_id")
+    private Board board;
+
+    @ManyToOne
+    @JoinColumn(name = "comment_id")
+    private Comment comment;
+
+}
