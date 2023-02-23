2a72d404acbe05e3fc1c6dd949b01a4920515cd1
doyeonKim <0915dy@gmail.com>
2023-02-23 21:19:57 +0900

    refactor: 예외처리시 log 찍기
    @Slf4j 이용해서 예외처리할 때 에러 메시지를 log로 찍도록 구현

--- src/main/java/com/sparta/board/exception/CustomExceptionHandler.java
+++ src/main/java/com/sparta/board/exception/CustomExceptionHandler.java
@@ -3,23 +3,27 @@ package com.sparta.board.exception;
 import com.sparta.board.common.ApiResponseDto;
 import com.sparta.board.common.ErrorResponse;
 import com.sparta.board.common.ResponseUtils;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+@Slf4j
 @ControllerAdvice
 public class CustomExceptionHandler {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<ApiResponseDto<ErrorResponse>> methodValidException(MethodArgumentNotValidException e) {
         ErrorResponse response = ErrorResponse.of(e.getBindingResult());
+        log.error(response.getMessage());
         return ResponseEntity.badRequest().body(ResponseUtils.error(response));
     }
 
     @ExceptionHandler(RestApiException.class)
     public ResponseEntity<ApiResponseDto<ErrorResponse>> customException(RestApiException e) {
         ErrorResponse response = ErrorResponse.of(e.getErrorType());
+        log.error(response.getMessage());
         return ResponseEntity.badRequest().body(ResponseUtils.error(response));
     }
 
